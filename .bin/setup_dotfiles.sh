ff886e2175bdb2e124b1122a7576de50a73ef2e5
burnbrian <98911252+burnbrian@users.noreply.github.com>
2024-02-14 08:42:28 -0500

    Updated installer

--- .bin/setup_dotfiles.sh
+++ .bin/setup_dotfiles.sh
@@ -4,22 +4,21 @@
 git clone --bare https://github.com/burnbrian/mydotfiles.git $HOME/.mydotfiles
 
 # Set naked git function
-function config {
+function mdf {
    git --git-dir=$HOME/.mydotfiles/ --work-tree=$HOME $@
 }
 
+# Do not show untracked files
+mdf config status.showUntrackedFiles no
+
 # Checkout branch main
-config checkout
+mdf checkout &2>/dev/null
 if [ $? = 0 ]; then
   printf %s "Repo checked out..."
   else
     printf %s "Backing up current dotfiles..."
-    mkdir -p $HOME/.config-backup
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    mkdir -p $HOME/.mdf-backup
+    mdf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.mdf-backup/{}
+    printf %s "Copying git dotfiles..."
+    mdf checkout
 fi;
-
-# Checkout and do not show untracked files
-# Also set in .gitconfig
-
-config checkout
-config config status.showUntrackedFiles no
